ff51c06bceb1a059c2f74ba9c4289ba0fb746e8f
xiazihang <zhxia@thoughtworks.com>
2017-01-06 00:26:51 +0800

    fix

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,7 @@
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.ErrorPageFilter;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
